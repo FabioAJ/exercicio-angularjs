0e425fed399eaf590321d4017713d311e8f50261
FabioAJ <fabioaugusto.aj@gmail.com>
2019-03-22 17:21:05 -0300

    Implementa layout inicial e controller

---
+++ scripts/primeiro-controller.js
@@ -0,0 +1,13 @@
+angular.module('aplicacao').controller('PrimeiroController', function($scope) {
+    $scope.name = 'amigo';
+    $scope.iniciado = true;
+    $scope.suricatos = [
+        'Suricato Marrom', 'Suricato Bege', 'Suricato Rei', 'Suricato Femea'
+    ];
+    $scope.finalizar = function() {
+        $scope.iniciado = false;
+    };
+    $scope.iniciar = function() {
+        $scope.iniciado = true;
+    }
+});
