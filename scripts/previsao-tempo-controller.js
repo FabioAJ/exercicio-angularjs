6cf67b258ad147e3e726c3cef5c8ac1e8e341270
FabioAJ <fabioaugusto.aj@gmail.com>
2019-03-25 17:45:22 -0300

    Implementa controller da previsao do tempo com api

---
+++ scripts/previsao-tempo-controller.js
@@ -0,0 +1,13 @@
+angular.module('aplicacao').controller('PrevisaoTempoController', function($scope, $resource) {
+    var WeatherChannel = $resource('https://api.openweathermap.org/data/2.5/weather?id=3445831&units=metric&APPID=cb736063c590f2abe550a94e94645939'); 
+    
+    var getWeather = function() {
+        WeatherChannel.get().$promise.then(function(response) {
+            $scope.tempo = response;
+            console.log(response);
+        }, function(promise) {
+            alert("Erro ao requisitar o recurso!");
+        })
+    }
+    getWeather();
+});
